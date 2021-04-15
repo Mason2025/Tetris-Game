1b5532c7213ee3d0bdea5eea4fd08a2e2880a13b
Basicpython <39888296+Basicpython@users.noreply.github.com>
2021-04-15 15:37:08 -0500

    Update Tetris.py
    A simple outline of the game's proto-code

--- Tetris.py
+++ Tetris.py
@@ -1,2 +1,17 @@
-# Mason, Casey, David, Logan
-f
+# Team: 8-bit
+# Members: Mason, David, Casey, Logan
+
+# start screen that awaits input to begin the game
+
+# screen that displays highscore and current score, waits for input to play again
+
+# The Tetris Game
+
+# highscore and current score initialization
+
+# the layout of the grid
+
+# section of code devoted to input from buttons
+
+# the blocks
+
