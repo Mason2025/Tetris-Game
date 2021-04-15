994ddfb445e34504d32cb3acd821e8bd6f4e4af7
Basicpython <39888296+Basicpython@users.noreply.github.com>
2021-04-15 16:11:08 -0500

    Update Tetris.py
    Added: inheritance from tkinter, the game class, a class for blocks, and instantiation of both the high score and current score.

--- Tetris.py
+++ Tetris.py
@@ -1,17 +1,31 @@
 # Team: 8-bit
 # Members: Mason, David, Casey, Logan
 
+# inheritance from Tkinter
+from tkinter import *
+
 # start screen that awaits input to begin the game
 
 # screen that displays highscore and current score, waits for input to play again
 
-# The Tetris Game
+# Class for the game
+class Game(Frame):
 
+    def __init__(self, parent):
+        Frame.__init__(self,parent)
+        
 # highscore and current score initialization
+highscore = 0
+currentScore = 0
 
 # the layout of the grid
 
 # section of code devoted to input from buttons
 
-# the blocks
+# classes for the blocks
+
+# Block super class
+class Block():
+
+    def __init__(self, color):
 
