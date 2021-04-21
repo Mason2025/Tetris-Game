4678378db7cb5ebcd98e676e80e34605da007a2a
Basicpython <39888296+Basicpython@users.noreply.github.com>
2021-04-21 10:33:50 -0500

    Update Tetris.py
    Added rotation to the shapes, added the creation of the window

--- Tetris.py
+++ Tetris.py
@@ -139,6 +139,23 @@ class Piece(object):
         self.color = shape_colors[shapes.index(shape)]
         self.rotation = 0
 
+# rotating shapes
+def rotateShape(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+ 
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+ 
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+ 
+    return positions
+
+
 # display the given message (Used for game over)
 def message(text, size, color, surface):
     font = pygame.font.SysFont('timesnewroman', size, bold=True)
@@ -193,6 +210,24 @@ def showGrid(surface, row, col):
         pygame.draw.line(surface, (128,128,128), (sx, sy+ i*30), (sx + gridWidth, sy + i * 30))  # horizontal lines
         for j in range(col):
             pygame.draw.line(surface, (128,128,128), (sx + j * 30, sy), (sx + j * 30, sy + gridHeight))  # vertical lines
+
+# define the window
+def createWindow(surface):
+    surface.fill("black")
+    # Tetris Title
+    font = pygame.font.SysFont('timesnewroman', 60)
+    label = font.render('TETRIS', 1, "red")
+ 
+    surface.blit(label, (upperLeftX + gridWidth / 2 - (label.get_width() / 2), 30))
+ 
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (upperLeftX + j* 30, upperLeftY + i * 30, 30, 30), 0)
+ 
+    # create grid
+    showGrid(surface, 20, 10)
+    pygame.draw.rect(surface, "white", (upperLeftX, upperLeftY, gridWidth, gridHeight), 5)
+
 
 # section of code devoted to input from buttons
         
